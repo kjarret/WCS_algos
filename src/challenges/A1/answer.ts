906ecd86aa1f827be07b2201e8279ab5d48ee2f9
kjarret <jarretkenny@gmail.com>
2024-01-29 14:36:30 +0100

    Initial commit

---
+++ src/challenges/A1/answer.ts
@@ -0,0 +1,30 @@
+/**
+ * In this challenge, you must sort all ads by their price (cheapest first). If some of them
+ * have the same price, you should sort by their title alphabetically (A to Z)
+ *
+ * @param ads Unsorted list of ads
+ * @returns Sorted list of ads
+ */
+
+// ↓ uncomment bellow lines and add your response!
+/*
+export default function ({ ads }: { ads: Ad[] }): Ad[] {
+  return ads.sort((adA, adB) => {
+    if (adA.price < adB.price) {
+      return -1;
+    } else if (adA.price > adB.price) {
+      return 1;
+    }
+    if (adA.price === adB.price) {
+      return adA.title.localeCompare(adB.title);
+    }
+    return 0;
+  });
+}
+*/
+// used interfaces, do not touch
+export interface Ad {
+  title: string;
+  price: number;
+  tags: string[];
+}
---
+++ src/challenges/B1/answer.ts
@@ -0,0 +1,31 @@
+/**
+ * In this challenge, you must sort messages chronologically (oldest to latest) based on
+ * their sentAt prop. If some messages have the same sentAt, then they should be
+ * sorted by their content length (shortest first)
+ *
+ * @param messages Unsorted list of messages
+ * @returns Sorted list of messages
+ */
+
+// ↓ uncomment bellow lines and add your response!
+/*
+export default function ({ messages }: { messages: Message[] }): Message[] {
+  return messages.sort((messageA, messageB) => {
+    if (messageA.sentAt === messageB.sentAt) {
+      if (messageA.content.length > messageB.content.length) {
+        return 1;
+      } else {
+        return -1;
+      }
+    }
+    return messageA.sentAt.localeCompare(messageB.sentAt);
+  });
+}
+*/
+
+// used interfaces, do not touch
+export interface Message {
+  content: string;
+  sentBy: string;
+  sentAt: string;
+}
